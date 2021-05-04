4a8d5174a2dcf0f4874379cc4fc98026bf43a142
Sam <samreilly132@icloud.com>
2021-05-04 11:26:24 +0200

    Add project files.

---
+++ AddingItems.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodilityChallenges
+{
+    class AddingItems
+    {
+        public int solution(int[] A)
+        {
+            int K = A[0];
+            int counter = 0;
+            int maxCapacity = 5000;
+
+            int remainderCap = maxCapacity - K;
+            Array.Sort(A);
+
+            for (int i = 0; i < A.Length -1; i++)
+            {
+                remainderCap -= A[i];
+                counter++;
+                if (A[i] > remainderCap)
+                {
+                    break;
+                }
+            }
+            return counter;
+        }
+
+/*        static void Main(string[] args)
+        {
+            AddingItems ai = new AddingItems();
+            Console.WriteLine(ai.solution(new int[] { 4850, 100, 30, 30, 100, 50, 100 })); // 3
+            Console.WriteLine(ai.solution(new int[] { 4650, 150, 150, 150 })); // 2
+        }*/
+    }
+}
---
+++ FibonacciSequence.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodilityChallenges
+{
+    class FibonacciSequence
+    {
+
+        public int solution(int X)
+        {
+            var fibonnaciNumbers = fibonnaciNumbersArray();
+
+            // check if X is a fib number
+            if (fibonnaciNumbers.Contains(X))
+                return 0;
+
+            else
+            {
+                for (int i = 0; i < fibonnaciNumbers.Count - 2; i++)
+                {
+                    var current = fibonnaciNumbers[i];
+                    var next = fibonnaciNumbers[i + 1];
+
+                    // X is between two Fib numbers
+                    if (current < X && X < next)
+                    {
+                        // Get the difference it is from each
+                        var distanceFromCurr = X - current;
+                        var distanceFromNext = next - X;
+
+                        // return the minimum of the two 
+                        return Math.Min(distanceFromCurr, distanceFromNext);
+                    }
+                }
+            }
+            return 999;
+        }
+         
+
+        private List<int> fibonnaciNumbersArray()
+        {
+            // create a map for fib numbers. 
+            var fibNums = new List<int>();
+
+            fibNums.Add(0);
+            fibNums.Add(1);
+
+            int next = 1;
+            int i = 2;
+
+            while (next < 1000000)
+            {
+                next = fibNums[i - 2] + fibNums[i - 1];
+                fibNums.Add(next);
+                i++;
+            }
+
+            return fibNums;
+        }
+ 
+
+        static void Main(string[] args)
+        {
+            FibonacciSequence fs = new FibonacciSequence();
+            Console.WriteLine(fs.solution(15)); // 2
+/*            Console.WriteLine(fs.solution(13)); // 0
+            Console.WriteLine(fs.solution(100)); // 11*/
+        }
+
+    }
+}
