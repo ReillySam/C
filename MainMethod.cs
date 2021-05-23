900330f28856410861d194c24c4d29a30a5de1b0
Sam <samreilly132@icloud.com>
2021-05-23 16:07:08 +0200

    Prime & Composite Numbers

--- MainMethod.cs
+++ MainMethod.cs
@@ -9,41 +9,47 @@ namespace CodilityChallenges
     {
         static void Main(string[] args)
         {
-            /*            AddingItems ai = new AddingItems();
-                        Console.WriteLine(ai.solution(new int[] { 4850, 100, 30, 30, 100, 50, 100 })); // 3
-                        Console.WriteLine(ai.solution(new int[] { 4650, 150, 150, 150 })); // 2
+            /*  AddingItems ai = new AddingItems();
+            Console.WriteLine(ai.solution(new int[] { 4850, 100, 30, 30, 100, 50, 100 })); // 3
+            Console.WriteLine(ai.solution(new int[] { 4650, 150, 150, 150 })); // 2
 
-                        FibonacciSequence fs = new FibonacciSequence();
-                        Console.WriteLine(fs.solution(15)); // 2
+            FibonacciSequence fs = new FibonacciSequence();
+            Console.WriteLine(fs.solution(15)); // 2
 
-                        NumOfDiscIntersections nodi = new NumOfDiscIntersections();
-                        Console.WriteLine(nodi.Solution(new int[] { 1, 5, 2, 1, 4, 0 })); // 11
+            NumOfDiscIntersections nodi = new NumOfDiscIntersections();
+            Console.WriteLine(nodi.Solution(new int[] { 1, 5, 2, 1, 4, 0 })); // 11
 
-                        Nesting n = new Nesting();
-                        Console.WriteLine(n.Solution("(()(())())")); // 1
-                        Console.WriteLine(n.Solution("())")); // 0 
+            Nesting n = new Nesting();
+            Console.WriteLine(n.Solution("(()(())())")); // 1
+            Console.WriteLine(n.Solution("())")); // 0 
 
-                        Brackets b = new Brackets();
-                        Console.WriteLine(b.Solution("{[()()]}"));
-                        Console.WriteLine(b.Solution("([)()]"));
+            Brackets b = new Brackets();
+            Console.WriteLine(b.Solution("{[()()]}"));
+            Console.WriteLine(b.Solution("([)()]"));
 
-                        StoneWall sw = new StoneWall();
-                        Console.WriteLine(sw.Solution(new int[] { 8, 8, 5, 7, 9, 8, 7, 4, 8 }));
-
-
-                        EquiLeader el = new EquiLeader();
-                        Console.WriteLine(el.Solution(new int[] { 4, 3, 4, 4, 4, 2 })); // 2
+            StoneWall sw = new StoneWall();
+            Console.WriteLine(sw.Solution(new int[] { 8, 8, 5, 7, 9, 8, 7, 4, 8 }));
 
 
-                        MaxProfit mp = new MaxProfit();
-                        Console.WriteLine(mp.Solution(new int[] { 23171, 21011, 21123, 21366, 21013, 21367 })); // 356
-                        Console.WriteLine(mp.Solution(new int[] { 5, -7, 3, 5, -2, 4, -1 })); // 3, 5, -2, 4
+            EquiLeader el = new EquiLeader();
+            Console.WriteLine(el.Solution(new int[] { 4, 3, 4, 4, 4, 2 })); // 2
 
-                        MaxSliceSum mss = new MaxSliceSum();
-                        Console.WriteLine(mss.Solution(new int[] { 2, 3, -6, 4, 0 })); // 5
-            */
+
+            MaxProfit mp = new MaxProfit();
+            Console.WriteLine(mp.Solution(new int[] { 23171, 21011, 21123, 21366, 21013, 21367 })); // 356
+            Console.WriteLine(mp.Solution(new int[] { 5, -7, 3, 5, -2, 4, -1 })); // 3, 5, -2, 4
+
+            MaxSliceSum mss = new MaxSliceSum();
+            Console.WriteLine(mss.Solution(new int[] { 2, 3, -6, 4, 0 })); // 5
+            
             MaxDoubleSliceSum mdss = new MaxDoubleSliceSum();
             Console.WriteLine(mdss.Solution(new int[] { 3, 2, 6, -1, 4, 5, -1, 2 })); // 5
+
+            */
+
+            MinPerimeterRectangle mpr = new MinPerimeterRectangle();
+            Console.WriteLine(mpr.Solution( 30 )); // 22
+            Console.WriteLine(mpr.Solution(36)); // 24
 
         }
     }
---
+++ MinPerimeterRectangle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodilityChallenges
+{
+    class MinPerimeterRectangle
+    {
+        public int Solution(int N)
+        {
+            // get divisors of N
+            // min perimeter of divisors, min must be the closest element(s) / closest square of N
+            // int min_perimeter = Int32.MaxValue;
+
+            int a;
+
+            for (a = (int)Math.Sqrt(N); a > 0; a--)
+            {
+                if (N % a == 0)
+                {
+                    break;
+                }   
+            }
+
+            return 2 * (a + N / a);
+        }
+
+    }
+}
